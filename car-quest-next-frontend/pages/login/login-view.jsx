22f17df7750f7af6c4b3f3aff8fd95b95069823a
Akthab <mhmdakthab6@gmail.com>
2023-12-11 14:34:56 +0530

    fix(): added styles and snackbar

--- car-quest-next-frontend/pages/login/login-view.jsx
+++ car-quest-next-frontend/pages/login/login-view.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import styles from './login-view.module.css';
 import LoginFormController from './components/login-form/login-form-controller';
+import { Grid } from '@mui/material';
 
 export default function LoginView() {
 	return (
-		<div className={styles.screen}>
-			<div className={styles.left_half}>
+		<Grid container className={styles.screen}>
+			<Grid
+				item
+				// xs={6}
+				sm={6}
+				className={styles.left_half}
+				sx={{ display: { xs: 'none', sm: 'flex' } }}
+			>
 				<div className={styles.information_cont}>
 					<h1 className={styles.font_style}>
 						Stay Ahead of Repairs: Optimize Vehicle Performance With Our
 						Maintainence App
 					</h1>
 				</div>
-			</div>
-			<div className={styles.right_half}>
+			</Grid>
+			<Grid item xs={12} sm={6} className={styles.right_half}>
 				<div className={styles.login_cont}>
 					<LoginFormController />
 				</div>
-			</div>
-		</div>
+			</Grid>
+		</Grid>
 	);
 }
