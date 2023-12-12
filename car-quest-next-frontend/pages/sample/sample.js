c0ab4ba98990fabb7a40538b9a8c27a3d0135d67
Akthab <mhmdakthab6@gmail.com>
2023-12-12 23:16:05 +0530

    feat(): added side drawer for easier navigation

---
+++ car-quest-next-frontend/pages/sample/sample.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from '../../layout/MainLayout';
+
+const SamplePage = () => {
+	return (
+		<>
+			<MainLayout />
+		</>
+	);
+};
+
+export default SamplePage;
